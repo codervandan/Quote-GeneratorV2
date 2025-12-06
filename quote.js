5fe43ef2b946379fb51c8f18759925a3798cc164
codervandan <devopdan@protonmail.com>
2025-12-06 15:56:11 -0800

    refactored final product

---
+++ quote.js
@@ -0,0 +1,22 @@
+const localQuotes = [
+  {
+    "text": "These are my quotes. I'm a great programmer",
+    "author": "Thomas Edison"
+  },
+  {
+    "text": "I will become the most profitable in all my businesses.",
+    "author": "Yogi Berra"
+  },
+  {
+    "text": "Day Trading is a mindset.",
+    "author": "Abraham Lincoln"
+  },
+  {
+    "text": "Programming will give you a future.",
+    "author": "Johann Wolfgang von Goethe"
+  },
+  {
+    "text": "Fate is in your hands and no one elses",
+    "author": "Byron Pulsifer"
+  }
+]
